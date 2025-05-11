6c7d6e0adfdd718ccd91556c1c93c1b3bc5c102c
kharlamovilya <iluhasupergames@gmail.com>
2025-05-11 18:00:17 +0800

    init

--- src/pages/HomeCover.jsx
+++ src/pages/HomeCover.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import runImage from './assets/run.jpg';
+import runImage from '../assets/run.jpg';
 const HomeCover = () => {
     return (
         <section
