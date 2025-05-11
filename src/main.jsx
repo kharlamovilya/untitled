a6093d28db771f338398ecdbfb5767aa11106566
kharlamovilya <iluhasupergames@gmail.com>
2025-05-11 17:42:54 +0800

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom'; // instead of BrowserRouter
-
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
